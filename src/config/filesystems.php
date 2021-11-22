dc9c847acce8f082944afc64627f36727ff651de
nousi <nousidayo@gmail.com>
2021-11-22 18:12:11 +0900

    .env minioの環境変数を追加

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -65,6 +65,17 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'minio' => [
+            'driver'                  => 's3',
+            'key'                     => env('MINIO_ACCESS_KEY_ID'),
+            'secret'                  => env('MINIO_SECRET_ACCESS_KEY'),
+            'region'                  => env('MINIO_DEFAULT_REGION'),
+            'bucket'                  => env('MINIO_BUCKET'),
+            'url'                     => env('MINIO_URL'),
+            'endpoint'                => env('MINIO_ENDPOINT'),
+            'use_path_style_endpoint' => env('MINIO_PATH_STYLE_ENDPOINT'),
+        ]
+
     ],
 
 ];
